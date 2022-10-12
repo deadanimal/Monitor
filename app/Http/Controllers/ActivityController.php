77291d0f86f0d27cc5fb83f82881023aa61f95ca
Afeez Aziz <afeezaziz@gmail.com>
2022-10-12 17:14:06 +0800

    dsa

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -129,11 +129,8 @@ class ActivityController extends Controller
         $activity->pekerja_id = (int)$request->pekerja_id;
         $activity->pemeriksa_id = (int)$request->pemeriksa_id;
         $activity->pengesah_id = (int)$request->pengesah_id;
-        $activity->organisasi_id = $request->organisasi->id;
         $activity->tarikh_rancang = $request->tarikh_rancang;
         $activity->deskripsi = $request->deskripsi;
-        $activity->projek_id = $request->projek->id;
-        $activity->supervisor_id = $request->supervisor->id;
 
         $activity->save();
 
