aba60773dba6c6a5c0e6869d4408761658e2d882
Afeez Aziz <afeezaziz@gmail.com>
2022-10-12 20:15:39 +0800

    321

--- resources/views/projek/satu.blade.php
+++ resources/views/projek/satu.blade.php
@@ -352,7 +352,7 @@
 													</div>		
 													<div class="mb-3 col-md-6">
 														<label for="title">Jumlah</label>
-														<input type="number" class="form-control" id="jumlah" name="jumlah" required>
+														<input type="number" class="form-control" step='0.01' id="jumlah" name="jumlah" required>
 													</div>	
 													<div class="mb-3 col-md-6">
 														<label>Tarikh</label>                                               
